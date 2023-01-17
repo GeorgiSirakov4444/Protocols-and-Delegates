1f74837e4a4f14f5422004d8103d8b43cab61dad
Georgi Sirakov <georgi.sirakov74@gmail.com>
2023-01-17 21:57:07 +0200

    Delegates

--- Protocols and Delegates/main.swift
+++ Protocols and Delegates/main.swift
@@ -1,11 +1,52 @@
-//
-//  main.swift
-//  Protocols and Delegates
-//
-//  Created by Georgi Sirakov on 17.01.23.
-//
+protocol AdvancedLifeSupport {
+    func PerformCPR()
+}
 
-import Foundation
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assesSituation() {
+        print("Can you tell me, what happened ?")
+    }
+    
+    func medicalEmergency() {
+        delegate?.PerformCPR()
+    }
+}
 
-print("Hello, World!")
+struct Paramedic: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func PerformCPR() {
+        print("30 chesst compressions, per minute.")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func PerformCPR() {
+        print("30 chesst compressions, per minute.")
+    }
+    func useStethescope() {
+        print("Listening for heart sounds.")
+    }
+}
+
+class Surgeon: Doctor {
+    override func PerformCPR() {
+        super.PerformCPR()
+            print("Sign Stay Alive, by BeeJees !")
+        
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let pete = Paramedic(handler: emilio)
+let angela = Surgeon(handler: emilio)
+
+emilio.assesSituation()
+emilio.medicalEmergency()
 
